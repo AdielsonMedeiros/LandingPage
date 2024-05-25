52ca6a7b89be578ec3bb2af0a26d2ff3d8aa3dcd
Adielson Medeiros <medeirosadielson@gmail.com>
2024-05-25 00:04:42 -0300

    footer responsivo

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,4 +3,21 @@ $(document).ready(function(){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
-})
+
+    //efeito rolagem
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function() {
+        const header = $('header');
+        let activeSectionIndex = 0;
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        
+        if(scrollPosition <= 0){
+            header.css('box-shadow', 'none');
+        } else{
+            header.css('box-shadow', '5px 1px 5px rgba(0,0,0, 0.1');
+        }
+        
+    });
+});
